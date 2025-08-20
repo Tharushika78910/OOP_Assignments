3ecdb6173fb380b340720c046b121d43d1f3d114
Dilmi Tharushika <Dilmi.MerenchiKankanamge@metropolia.fi>
2025-08-20 11:31:08 +0300

    First Commit Task 1

---
+++ Assignment_1_2/src/Task_1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Task_1 {
+    public static void main(String[] args)
+    {
+        Scanner user_input = new Scanner(System.in);
+
+        System.out.print("Enter the temperature in Celsius: ");
+        double temp_Cel = user_input.nextInt();
+
+        double temp_Fah = (temp_Cel * 9/5) + 32;
+
+        System.out.println("The temperature in Fahrenheit is " + temp_Fah);
+
+    }
+}
