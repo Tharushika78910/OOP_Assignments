b591e7a180d722dfb88108d61af68181a771545e
Dilmi Tharushika <Dilmi.MerenchiKankanamge@metropolia.fi>
2025-10-06 15:58:27 +0300

    First Commit

---
+++ Assignment_6_3/src/main/java/Main.java
@@ -0,0 +1,8 @@
+
+import view.VirtualPetApplication;
+
+public class Main {
+    public static void main(String[] args) {
+        VirtualPetApplication.launch(VirtualPetApplication.class);
+    }
+}
